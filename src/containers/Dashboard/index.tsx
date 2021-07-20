46e21d40a97edf48e2f0de15b4c4516579f85200
Ben Trinh MacbookPro <bt@multi-it.dk>
2021-07-20 16:42:00 +0700

    [DASHBOARD] - Change label and close modal when finish submit data to api

--- src/containers/Dashboard/index.tsx
+++ src/containers/Dashboard/index.tsx
@@ -6,16 +6,16 @@ import { useEffect } from 'react'
 import { GET_EMPLOYEE, UPDATE_PAGINATION } from 'src/redux/reducers/employee'
 import { DataGrid } from '@material-ui/data-grid'
 import {
-    Pagination,
-    Select,
-    MenuItem,
-    Grid,
-    Container,
-    Button,
-    Box,
-    Modal,
-    Typography,
-    TextField,
+	Pagination,
+	Select,
+	MenuItem,
+	Grid,
+	Container,
+	Button,
+	Box,
+	Modal,
+	Typography,
+	TextField,
 } from '@material-ui/core'
 import { Add } from '@material-ui/icons'
 import columns from 'src/common/DataGrid'
@@ -23,198 +23,204 @@ import style from './styles'
 import { useForm } from 'react-hook-form'
 import axios from 'src/utils/axios'
 
+interface DataForm {
+	name: string,
+	email: string,
+	position: string
+}
 const DashboardComponent = React.memo((props: DashboardTypes) => {
-    const { dispatch, employees, pagination } = props,
-        { limit, page } = pagination,
-        [open, setOpen] = React.useState(false),
-        {
-            handleSubmit,
-            register,
-            formState: { errors },
-        } = useForm()
+	const { dispatch, employees, pagination } = props,
+		{ limit, page } = pagination,
+		[open, setOpen] = React.useState(false),
+		{
+			handleSubmit,
+			register,
+			formState: { errors },
+		} = useForm()
 
-    const handleOpen = () => {
-        setOpen(true)
-    }
+	const handleOpen = () => {
+		setOpen(true)
+	}
 
-    const handleClose = () => {
-        setOpen(false)
-    }
+	const handleClose = () => {
+		setOpen(false)
+	}
 
-    const getUsers = () => {
-        dispatch({
-            type: GET_EMPLOYEE,
-        })
-    }
+	const getUsers = () => {
+		dispatch({
+			type: GET_EMPLOYEE,
+		})
+	}
 
-    const onPageChange = (_: React.ChangeEvent<unknown>, p: number) => {
-        dispatch({
-            type: UPDATE_PAGINATION,
-            data: {
-                ...pagination,
-                page: p,
-            },
-        })
-    }
+	const onPageChange = (_: React.ChangeEvent<unknown>, p: number) => {
+		dispatch({
+			type: UPDATE_PAGINATION,
+			data: {
+				...pagination,
+				page: p,
+			},
+		})
+	}
 
-    const handleChange = (e: any) => {
-        dispatch({
-            type: UPDATE_PAGINATION,
-            data: {
-                ...pagination,
-                limit: e.target.value,
-            },
-        })
-    }
+	const handleChange = (e: any) => {
+		dispatch({
+			type: UPDATE_PAGINATION,
+			data: {
+				...pagination,
+				limit: e.target.value,
+			},
+		})
+	}
 
-    const onSubmit = async (data: any) => {
-        try {
-            await axios.post('/users', data)
-            getUsers()
-        } catch (err) {
-            console.log(err)
-        }
-    }
+	const onSubmit = async (data: DataForm) => {
+		try {
+			await axios.post('/users', data)
+			getUsers()
+			setOpen(false)
+		} catch (err) {
+			console.log(err)
+		}
+	}
 
-    useEffect(() => {
-        dispatch({
-            type: GET_EMPLOYEE,
-        })
-    }, [dispatch, limit, page])
+	useEffect(() => {
+		dispatch({
+			type: GET_EMPLOYEE,
+		})
+	}, [dispatch, limit, page])
 
-    return (
-        <Container>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Create new employee
-                    </Typography>
-                    <Typography component="div" id="modal-modal-description" sx={{ mt: 2 }}>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <TextField
-                                defaultValue=""
-                                error={Boolean(errors['name'])}
-                                fullWidth
-                                margin="dense"
-                                id="name"
-                                label="Name"
-                                variant="outlined"
-                                {...register('name', {
-                                    required: true,
-                                    minLength: 1,
-                                })}
-                            />
+	return (
+		<Container>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box sx={style}>
+					<Typography id="modal-modal-title" variant="h6" component="h2">
+						Create new employee
+					</Typography>
+					<Typography component="div" id="modal-modal-description" sx={{ mt: 2 }}>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<TextField
+								defaultValue=""
+								error={Boolean(errors['name'])}
+								fullWidth
+								margin="dense"
+								id="name"
+								label="Name"
+								variant="outlined"
+								{...register('name', {
+									required: true,
+									minLength: 1,
+								})}
+							/>
 
-                            <TextField
-                                defaultValue=""
-                                error={Boolean(errors['email'])}
-                                type="email"
-                                fullWidth
-                                margin="dense"
-                                id="email"
-                                label="email"
-                                variant="outlined"
-                                {...register('email', {
-                                    required: true,
-                                    minLength: 1,
-                                    pattern: {
-                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                        message: 'invalid email address',
-                                    },
-                                })}
-                            />
+							<TextField
+								defaultValue=""
+								error={Boolean(errors['email'])}
+								type="email"
+								fullWidth
+								margin="dense"
+								id="email"
+								label="Email"
+								variant="outlined"
+								{...register('email', {
+									required: true,
+									minLength: 1,
+									pattern: {
+										value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+										message: 'invalid email address',
+									},
+								})}
+							/>
 
-                            <TextField
-                                defaultValue=""
-                                error={Boolean(errors['position'])}
-                                fullWidth
-                                margin="dense"
-                                id="position"
-                                label="position"
-                                variant="outlined"
-                                {...register('position', {
-                                    required: true,
-                                    minLength: 1,
-                                })}
-                            />
+							<TextField
+								defaultValue=""
+								error={Boolean(errors['position'])}
+								fullWidth
+								margin="dense"
+								id="position"
+								label="Position"
+								variant="outlined"
+								{...register('position', {
+									required: true,
+									minLength: 1,
+								})}
+							/>
 
-                            <Button type="submit">Submit</Button>
-                        </form>
-                    </Typography>
-                </Box>
-            </Modal>
-            <div
-                style={{
-                    height: 82 + 52 * pagination.limit,
-                    width: 'auto',
-                    content: 'center',
-                    marginBottom: 10,
-                }}
-            >
-                <Typography component="h2">
-                    <b>EMPLOYEES</b>
-                </Typography>
-                <DataGrid
-                    rows={employees}
-                    columns={columns}
-                    loading={employees.length === 0}
-                    checkboxSelection
-                    disableSelectionOnClick
-                    pageSize={pagination.limit}
-                    hideFooterPagination={true}
-                    hideFooter={true}
-                    autoHeight={true}
-                />
-            </div>
-            <Grid container justifyContent="space-between" spacing={0}>
-                <Grid item xs={4}>
-                    <Button variant="outlined" onClick={handleOpen}>
-                        <Add color="primary" />
-                    </Button>
-                </Grid>
-                <Grid container justifyContent="flex-end" item xs={8} spacing={0}>
-                    <Select
-                        id="pageSize"
-                        variant="standard"
-                        autoWidth={true}
-                        margin="none"
-                        value={Number(pagination.limit)}
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={5}>5</MenuItem>
-                        <MenuItem value={10}>10</MenuItem>
-                        <MenuItem value={20}>20</MenuItem>
-                        <MenuItem value={30}>30</MenuItem>
-                        <MenuItem value={pagination.total}>All</MenuItem>
-                    </Select>
-                    <Pagination
-                        page={pagination.page}
-                        onChange={onPageChange}
-                        count={
-                            pagination.total === pagination.limit
-                                ? 1
-                                : parseInt(String(pagination.total / pagination.limit)) + 1
-                        }
-                        shape="rounded"
-                        classes={{
-                            ul: 'justify-end',
-                        }}
-                    />
-                </Grid>
-            </Grid>
-        </Container>
-    )
+							<Button type="submit">Submit</Button>
+						</form>
+					</Typography>
+				</Box>
+			</Modal>
+			<div
+				style={{
+					height: 82 + 52 * pagination.limit,
+					width: 'auto',
+					content: 'center',
+					marginBottom: 10,
+				}}
+			>
+				<Typography component="h2">
+					<b>EMPLOYEES</b>
+				</Typography>
+				<DataGrid
+					rows={employees}
+					columns={columns}
+					loading={employees.length === 0}
+					checkboxSelection
+					disableSelectionOnClick
+					pageSize={pagination.limit}
+					hideFooterPagination={true}
+					hideFooter={true}
+					autoHeight={true}
+				/>
+			</div>
+			<Grid container justifyContent="space-between" spacing={0}>
+				<Grid item xs={4}>
+					<Button variant="outlined" onClick={handleOpen}>
+						<Add color="primary" />
+					</Button>
+				</Grid>
+				<Grid container justifyContent="flex-end" item xs={8} spacing={0}>
+					<Select
+						id="pageSize"
+						variant="standard"
+						autoWidth={true}
+						margin="none"
+						value={Number(pagination.limit)}
+						onChange={handleChange}
+					>
+						<MenuItem value={5}>5</MenuItem>
+						<MenuItem value={10}>10</MenuItem>
+						<MenuItem value={20}>20</MenuItem>
+						<MenuItem value={30}>30</MenuItem>
+						<MenuItem value={pagination.total}>All</MenuItem>
+					</Select>
+					<Pagination
+						page={pagination.page}
+						onChange={onPageChange}
+						count={
+							pagination.total === pagination.limit
+								? 1
+								: parseInt(String(pagination.total / pagination.limit)) + 1
+						}
+						shape="rounded"
+						classes={{
+							ul: 'justify-end',
+						}}
+					/>
+				</Grid>
+			</Grid>
+		</Container>
+	)
 })
 
 DashboardComponent.displayName = 'Dashboard'
 const mapStateToProps = (state: Reducer) => ({
-    employees: state.employee.data,
-    pagination: state.employee.pagination,
+	employees: state.employee.data,
+	pagination: state.employee.pagination,
 })
 // @ts-ignore
 export default connect(mapStateToProps)(DashboardComponent)
